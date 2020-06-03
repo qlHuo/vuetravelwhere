8e4464c30f66e7b32faf06459fcc99e4b86e9df5
qlHuo <1322346367@qq.com>
2020-06-03 10:29:20 +0800

    project code init

--- src/main.js
+++ src/main.js
@@ -3,13 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入fastClick文件
+import fastClick from 'fastclick'
+// 引入初始化样式文件
+import './assets/styles/reset.css'
+// 解决移动端border一像素的问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
